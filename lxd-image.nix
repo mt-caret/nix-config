1fe84b83995ad1436021c963270bedc0dbdf64f6
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2020-07-14 13:51:11 +0900

    add nix base module for lxd containers

---
+++ lxd-image.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ... }:
+{
+  # https://github.com/NixOS/nixpkgs/issues/9735#issuecomment-500164017
+  systemd.services."console-getty".enable = false;
+  systemd.services."getty@".enable = false;
+
+  imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ];
+  networking.hostName = lib.mkDefault "nixos";
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+  };
+
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+}
