e9b0c967de860c0c4d2c9a1b2cb61744a05fc9e3
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2020-08-10 13:09:51 +0900

    artemis: move to new display config

--- home/gui.nix
+++ home/gui.nix
@@ -151,13 +151,13 @@ in
         case $(hostname) in
           artemis)
             xrandr \
-              --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+              --output DVI-D-0 --off \
               --output HDMI-0 --off \
-              --output DP-0 --off \
+              --output DP-0 --mode 3840x2160 --pos 1920x0 --rotate normal \
               --output DP-1 --off \
-              --output DP-2 --off \
+              --output DP-2 --mode 1920x1080 --pos 5767x0 --rotate normal \
               --output DP-3 --off \
-              --output DP-4 --primary --mode 3840x2160 --pos 1920x0 --rotate normal \
+              --output DP-4 --mode 1920x1080 --pos 0x0 --rotate normal \
               --output DP-5 --off
             ;;
           *)
